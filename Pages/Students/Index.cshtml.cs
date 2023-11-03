c8d1dc997a8671582e3968c393368da5a522d2a7
Lucas Coacci <48459327+CoacciLucas@users.noreply.github.com>
2023-11-02 22:30:36 -0300

    Add project files.

---
+++ Pages/Students/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorApp.Data;
+using RazorApp.Models;
+
+namespace RazorApp.Pages_Students
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorApp.Data.ApplicationDbContext _context;
+
+        public IndexModel(RazorApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Students != null)
+            {
+                Student = await _context.Students.ToListAsync();
+            }
+        }
+    }
+}
